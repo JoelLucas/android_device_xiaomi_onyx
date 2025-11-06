17348e206c53f3af3de8c12221d8631417ec5535
Ravinderrohilla <88099034+Ravinderrohilla@users.noreply.github.com>
2025-11-06 11:58:43 +0530

    Sign rom with my private keys

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ rm -rf hardware/xiaomi &&  git clone https://github.com/xiaomi-onyx-dev/android_
 git clone https://github.com/xiaomi-onyx-dev/android_device_xiaomi_onyx-kernel.git device/xiaomi/onyx-kernel
 git clone https://github.com/xiaomi-onyx-dev/proprietary_vendor_xiaomi_onyx.git vendor/xiaomi/onyx
 git clone https://github.com/xiaomi-onyx-dev/android_packages_apps_XiaomiDolby.git packages/apps/XiaomiDolby
-git clone https://github.com/Ravinderrohilla/priv-keys.git vendor/evolution-priv/keys
+git clone https://github.com/Ravinderrohilla/priv-keys.git vendor/lineage-priv/keys
 git clone https://gitea.com/goziko32/vendor_gcam.git vendor/mgc
