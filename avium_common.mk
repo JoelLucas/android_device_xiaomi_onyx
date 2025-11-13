4581dcbda6abcc93149408972d03e6742c4fc83e
Zarathos30 <vale.rigni@gmail.com>
2025-11-13 20:07:28 +0100

    Aggiornamento lineage_onyx.mk e nuovo avium_common.mk per AviumUI

---
+++ avium_common.mk
@@ -0,0 +1,13 @@
+AVIUM_BUILDTYPE := Unofficial
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := PICO
+
+# Google IME
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+# Maintainer (il tuo nome che apparirà in Impostazioni -> Info telefono)
+AVIUM_MAINTAINER := Zarathos30
