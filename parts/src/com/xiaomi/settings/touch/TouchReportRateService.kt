beef481def8b07cdbaf8b9444b8355976a008b3b
AdarshGrewal <adarshgrewal@gmail.com>
2026-04-26 16:25:19 +0530

    works fine so far 6
    Change-Id: I930d51de93d15e8701d778404b13ffca772f910e

--- parts/src/com/xiaomi/settings/touch/TouchReportRateService.kt
+++ parts/src/com/xiaomi/settings/touch/TouchReportRateService.kt
@@ -105,14 +105,11 @@ class TouchReportRateService : Service() {
             if (DEBUG) Log.d(TAG, "applyReportRate: $value")
 
             if (value == 1) {
-                // Match stock order from kmsg traces.
-                TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_REPORT_RATE, RATE_HIGH)
                 TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_GAME_MODE, 1)
                 TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_ACTIVE_MODE, 1)
             } else {
                 TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_ACTIVE_MODE, 0)
                 TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_GAME_MODE, 0)
-                TouchFeatureWrapper.setModeValue(TOUCH_ID, MODE_REPORT_RATE, RATE_NORMAL)
             }
         }
 
